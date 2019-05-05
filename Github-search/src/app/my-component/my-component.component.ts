a76d4e0273ede4b2a806ee41cc2f28a1c782ed9b
lwairore <kwairore@gmail.com>
2019-05-05 14:30:38 +0300

    Register HttpService, import HttpService, inject HttpService in my-details-component.ts

--- Github-search/src/app/my-component/my-component.component.ts
+++ Github-search/src/app/my-component/my-component.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-my-component',
   templateUrl: './my-component.component.html',
-  styleUrls: ['./my-component.component.css']
+  styleUrls: ['./my-component.component.css'],
+  providers: [HttpService],
 })
 export class MyComponentComponent implements OnInit {
 
---
+++ Github-search/src/app/my-details/my-details.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http-services/http.service';
+
+@Component({
+  selector: 'app-my-details',
+  templateUrl: './my-details.component.html',
+  styleUrls: ['./my-details.component.css'],
+  providers: [HttpService]
+})
+export class MyDetailsComponent implements OnInit {
+
+  constructor(public httpService: HttpService) { }
+
+  ngOnInit() {
+  }
+
+}
