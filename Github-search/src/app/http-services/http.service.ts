05121fb43d86e131954862e22581da3f9d1f98ee
lwairore <kwairore@gmail.com>
2019-05-05 13:59:37 +0300

    Intialize a myInfo variable of the same structure as Users class in http.service.ts

--- Github-search/src/app/http-services/http.service.ts
+++ Github-search/src/app/http-services/http.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Users } from '../classes/users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
+  myInfo: Users;
+
 
   constructor(public http:HttpClient) { }
+
+
+
+
+
 }
