8839043c811d6cc814195e9e496d7b1edf88e07e
lwairore <kwairore@gmail.com>
2019-05-05 13:56:05 +0300

    Create a class that has 6 properties: userName(optional), image, reposUrl, myPublicRepos, fullName(optional), nriefDescription(optional)

---
+++ Github-search/src/app/classes/users.ts
@@ -0,0 +1,3 @@
+export class Users {
+    constructor(public userName:string, public briefDescription:string, public image:string,public myReposUrl: string,public myPublicRepos:number ,public fullName?:string, ){}
+}
