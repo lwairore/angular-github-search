d6b1202178feb0c4523ada1806bd57ada04089a0
lwairore <kwairore@gmail.com>
2019-05-05 17:06:46 +0300

    Import HttpService, registered HttpService, inject HttpService as httpService to my-repository.components.ts

---
+++ Github-search/src/app/my-repository/my-repository.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http-services/http.service';
+
+@Component({
+  selector: 'app-my-repository',
+  templateUrl: './my-repository.component.html',
+  styleUrls: ['./my-repository.component.css'],
+  providers: [HttpService]
+})
+export class MyRepositoryComponent implements OnInit {
+
+  constructor(public httpService: HttpService) { }
+
+  ngOnInit() {
+  }
+
+}
