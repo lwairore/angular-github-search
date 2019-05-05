e756fdfdbf08aa2c2d38075439b8ba008060de53
lwairore <kwairore@gmail.com>
2019-05-05 13:34:42 +0300

    Initial commit

---
+++ Github-search/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
