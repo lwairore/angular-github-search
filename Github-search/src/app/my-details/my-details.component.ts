6d82fbcedec3e8938191186887bd99a3ab8a6299
lwairore <kwairore@gmail.com>
2019-05-05 14:34:18 +0300

    Initialize an empty variable myInfo and give  a type the same as of class Users. Import Users class and assign the value of this.httpService.myInfo to myInfo

--- Github-search/src/app/my-details/my-details.component.ts
+++ Github-search/src/app/my-details/my-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http-services/http.service';
+import { Users } from '../classes/users';
 
 @Component({
   selector: 'app-my-details',
@@ -8,12 +9,12 @@ import { HttpService } from '../http-services/http.service';
   providers: [HttpService]
 })
 export class MyDetailsComponent implements OnInit {
-
+  myInfo:Users
   constructor(public httpService: HttpService) { }
 
   ngOnInit() {
     this.httpService.myDetails();
-    
+    this.myInfo = this.httpService.myInfo
   }
 
 }
