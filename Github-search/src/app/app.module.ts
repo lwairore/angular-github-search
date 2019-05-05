8ec8f0dd75115d75921fa7ed6cb5933acdfbbc8b
lwairore <kwairore@gmail.com>
2019-05-05 15:34:35 +0300

    Import FormsModule and add it to the imports items

--- Github-search/src/app/app.module.ts
+++ Github-search/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyComponentComponent } from './my-component/my-component.component';
+import { MyDetailsComponent } from './my-details/my-details.component';
+import { OtherUsersComponent } from './other-users/other-users.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent
+    MyDetailsComponent,
+    OtherUsersComponent,
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
