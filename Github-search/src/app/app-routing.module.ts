f7276b2c27e9d38e4d07410d203929b913133e74
lwairore <kwairore@gmail.com>
2019-05-05 15:52:32 +0300

    Import; OtherUsersComponent, MyDetailsComponent and added them to routes variable

--- Github-search/src/app/app-routing.module.ts
+++ Github-search/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OtherUsersComponent } from './other-users/other-users.component';
+import { MyDetailsComponent } from './my-details/my-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: OtherUsersComponent },
+  { path: 'landingpage', component: MyDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
